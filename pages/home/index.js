bfeb30d7e376fe58fc177c2b347729992795baea
manikangkandas <manikangkan.das@outlook.com>
2022-09-10 14:18:24 +0530

    feat: dashboard home page configure

---
+++ pages/home/index.js
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className="h-full grid place-content-center space-y-4">
+      <h1>Good morning, Manikangkan</h1>
+      <p>
+        Welcome to Rubrica, before moving forward please go through the
+        following steps -
+      </p>
+      <ul>
+        <li>1. Create a rubric</li>
+        <li>2. Create a student</li>
+        <li>3. Assign a rubric to a student</li>
+        <li>4. Start grading</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
