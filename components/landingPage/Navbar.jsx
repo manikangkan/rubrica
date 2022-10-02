45c4ceae4f51cbf738cfd67c2b4ec6296ec68f8f
Manikangkan <manikangkan.das@outlook.com>
2022-10-02 10:34:50 +0530

    fix: index page update 2

--- components/landingPage/Navbar.jsx
+++ components/landingPage/Navbar.jsx
@@ -5,32 +5,21 @@ import jwtDecode from "jwt-decode";
 const Navbar = ({ user, setIsAuthModalOpen }) => {
   const isAdministratorLoggedIn =
     typeof window !== "undefined" && localStorage.getItem("rubrica");
-  const isEvoluterLoggedIn =
-    typeof window !== "undefined" && localStorage.getItem("rubrica-evoluter");
-
-  const handleEvoluterLogin = async () => {
-    try {
-      const id = jwtDecode(isEvoluterLoggedIn).userId;
-      const res = await axios.post(`${baseURL}/api/evoluter/login`, { id });
-      localStorage.setItem("rubrica-evoluter", res.data.token);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <nav className="flex items-center justify-between m-4">
       <Logo />
-      <p>
-        Admin logged in as <strong>{user?.name}</strong>
-      </p>
-      <div className="space-x-4">
+      {isAdministratorLoggedIn ? (
+        <p>
+          Admin logged in as <strong>{user?.name}</strong>
+        </p>
+      ) : (
         <button
           className="bg-slate-200 border-none text-black"
           onClick={() => setIsAuthModalOpen(true)}>
           Administrator login
         </button>
-      </div>
+      )}
     </nav>
   );
 };
