c339ef8fa9075e97878663bf5bb252d81f1d24cc
Manikangkan <manikangkan.das@outlook.com>
2022-10-16 09:29:25 +0530

    feat: react table config

--- pages/rubrics/index.js
+++ pages/rubrics/index.js
@@ -1,9 +1,35 @@
 import axios from "axios";
 import Link from "next/link";
+import { useMemo } from "react";
+import { useTable } from "react-table";
 import Search from "../../components/shared/Search.form";
 import baseURL from "../../utils/baseURL";
 
 const RubricsPage = ({ rubricsList }) => {
+  const notRequired = ["_id", "criteriaAndMarks", "__v"];
+  const filteredKeys = Object.keys(rubricsList[0]).filter(
+    (key) => !notRequired.includes(key)
+  );
+
+  const columns = useMemo(
+    () =>
+      filteredKeys.map((rubricKey, i) => {
+        return {
+          Header: rubricKey.charAt(0).toUpperCase() + rubricKey.slice(1),
+          accessor: rubricKey,
+        };
+      }),
+    []
+  );
+
+  const data = useMemo(() => rubricsList, []);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
   if (rubricsList.length === 0) {
     return <h1>No rubrics found</h1>;
   }
@@ -14,11 +40,49 @@ const RubricsPage = ({ rubricsList }) => {
         <Search />
         <button onClick={() => setAddStudent(!addStudent)}>Add</button>
       </div>
-      {rubricsList?.map((rubric) => (
+      {/* {rubricsList?.map((rubric) => (
         <Link href={`/rubrics/${rubric._id}`} key={rubric._id}>
           <div key={rubric._id}>{rubric.name}</div>
         </Link>
-      ))}
+      ))} */}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => {
+            const { key, ...restHeaderGroupProps } =
+              headerGroup.getHeaderGroupProps();
+            return (
+              <tr key={key} {...restHeaderGroupProps}>
+                {headerGroup.headers.map((column) => {
+                  const { key, ...restColumn } = column.getHeaderProps();
+                  return (
+                    <th key={key} {...restColumn}>
+                      {column.render("Header")}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {rows.map((row) => {
+            prepareRow(row);
+            const { key, ...restRowProps } = row.getRowProps();
+            return (
+              <tr key={key} {...restRowProps}>
+                {row.cells.map((cell) => {
+                  const { key, ...restCellProps } = cell.getCellProps();
+                  return (
+                    <td key={key} {...restCellProps}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
